2e64c2e9c4b10f20456bb3a2f74296bac0b09420
c-bec-k <cruthenbeck@gmail.com>
2023-08-04 10:56:44 -0400

    updated userCache

--- SimpleBudgetApp/Classes/UserCache.cs
+++ SimpleBudgetApp/Classes/UserCache.cs
@@ -7,7 +7,7 @@ namespace SimpleBudgetApp;
 public class UserCache
 {
   private record UserCacheItem(int UserId, DateTimeOffset Expiry);
-  private ConcurrentDictionary<string, UserCacheItem> _cache;
+  private readonly ConcurrentDictionary<string, UserCacheItem> _cache;
 #pragma warning disable IDE0052
   private readonly Timer _timer;
 #pragma warning restore IDE0052
@@ -26,22 +26,20 @@ public class UserCache
 
   public int GetUser(string hashVal)
   {
-    bool isFound = _cache.TryGetValue(hashVal, out UserCacheItem val);
-    if (!isFound) return 0;
-    return val.UserId;
+    var user = _cache.GetValueOrDefault(hashVal);
+    return user == null ? 0 : user.UserId;
   }
 
   private void CleanCache(object _)
   {
-    _cache = (ConcurrentDictionary<string, UserCacheItem>)_cache.Select(item => item.Value.Expiry > DateTimeOffset.Now);
-    // List<KeyValuePair<string, UserCacheItem>> Items = new();
-    // foreach (var item in _cache)
-    // {
-    //   if (item.Value.Expiry < DateTimeOffset.Now) Items.Add(item);
-    // }
-    // foreach (var item in Items)
-    // {
-    //   _cache.TryRemove(item);
-    // }
+    List<KeyValuePair<string, UserCacheItem>> Items = new();
+    foreach (var item in _cache)
+    {
+      if (item.Value.Expiry < DateTimeOffset.Now) Items.Add(item);
+    }
+    foreach (var item in Items)
+    {
+      _cache.TryRemove(item);
+    }
   }
 }
